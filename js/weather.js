58d2dd0bee368340861dfbe09b145647f2e3d9a1
neiljoshua <neiljoshua@me.com>
2016-07-03 14:19:36 -0400

    adding new div for user-search results

--- js/weather.js
+++ js/weather.js
@@ -27,36 +27,51 @@ $(document).ready(function() {
 	// 	});	    
 	// })
 	
+	function apiAutocomplete() {
+		var key = "http://api.wunderground.com/api/6d21846ad7649b70/forecast/q/zmw:94125.1.99999.json";
+		$.ajax({
+			url : key,
+			dataType: "jsonp",
+			success : function(data){
+				var cityResult = data.name;
+				var stateResult = data.z;
+				$('#user-location').html(cityResult+","+stateResult)
+
+			}
+		});
+	}
 
 	$('#user-location').on('change', function(){
-	 var city = $(this).val();
-	 console.log(city);
-	 var element = $('.weather-block');
+	 var userSearch = $(this).val();
+	 //console.log(city);
+	 apiAutocomplete();
+	 //var element = $('.weather-block');
 	 // console.log(element);
-	var key = '3af49458477b67aa';
+	//var key = '3af49458477b67aa';
 	// var state = 'NY';
-     var Weather = "http://api.wunderground.com/api/"+key+"/conditions/q/"+city+".json";
-        console.log(Weather); 
-    $.ajax({
-         url : Weather,
-         dataType : "jsonp",
-         success : function(results){
-           	var location = results.current_observation.display_location.full;
-           	console.log(results);
+    // var Weather = "http://api.wunderground.com/api/"+key+"/conditions/q/"+city+".json";
+    // var Weather = "http://autocomplete.wunderground.com/aq?query=usersearch";
+    // console.log(Weather); 
+    // $.ajax({
+    //      url : Weather,
+    //      dataType : "jsonp",
+    //      success : function(results){
+    //        	var location = results.current_observation.display_location.full;
+    //        	console.log(results);
+				// // console.log(location);
+				// var temp = results.current_observation.feelslike_f;
+				// var img = results.current_observation.icon_url;
+				// var desc = results.current_observation.weather;
+				// var wind = results.current_observation.wind_string;
+				// var test = element.find('.location').html(location);
+				// element.find('.location').html(location);
 				// console.log(location);
-				var temp = results.current_observation.feelslike_f;
-				var img = results.current_observation.icon_url;
-				var desc = results.current_observation.weather;
-				var wind = results.current_observation.wind_string;
-				var test = element.find('.location').html(location);
-				element.find('.location').html(location);
-				console.log(location);
-				element.find('.temp').html(temp);
-				element.find('.desc').html(desc);
-				element.find('.wind').html(wind);
-				element.find('.img').attr('src', img);
-           }                
-       });   
-	})
+				// element.find('.temp').html(temp);
+				// element.find('.desc').html(desc);
+				// element.find('.wind').html(wind);
+				// element.find('.img').attr('src', img);
+    //        }                
+    //    });   
+	});
 
 }); // End of doc ready. 	
