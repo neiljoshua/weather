8d3d4fe542cccc476ba0bc37e96eb2ba5b9039ef
neiljoshua <neiljoshua@me.com>
2016-07-10 10:16:42 -0400

    creating getWether after selecting city on list

--- js/weather.js
+++ js/weather.js
@@ -33,32 +33,68 @@ $(document).ready(function() {
 		console.log('Hola!');
 	});
 
-	$('#user-location').on('change', function(){
-		var city = $(this).val();
+	$('#user-location').on('change',function (){
+		getCities();
+		getWeather();
+	})
+
+
+	function getCities() {
+		var cityName ='';
+		var line = '';
+		var city = $('#user-location').val();
 		var element = $('#weather-results');
 		console.log(city);
-		var state = 'NY';
-		var key = '6d21846ad7649b70';
-		var Weather = "http://api.wunderground.com/api/"+key+"/conditions/q/"+state+"/"+city+".json";
+		var Weather = "http://autocomplete.wunderground.com/aq?query=" + city + "&h=0" ;
 	    console.log(Weather);
-	    console.log(city);
-    	 $.ajax({
-			  url : Weather,
-			  dataType : "jsonp",
-			  success : function(results) {
-			  var location = results.current_observation.display_location.full;
-			  console.log(location);
-			  var temp = results.current_observation.feelslike_f;
-			  console.log(temp);
-			  var img = results.current_observation.icon_url;
-			  element.find('#location-results').html(location);
-			  element.find('#icon-results').attr('src', img);
-			  element.find('#temp-results').html(temp);
-		  	}
-	    });
+	    var url = Weather + '&cb=?';
+			$.getJSON(url, function(jsonp){
+		    $('#json-results').html(JSON.stringify(jsonp, null, 1));
+		    var locations = JSON.stringify(jsonp, null, 1);
+		    locations = JSON.parse(locations);
+		    // console.log(locations);
+		    console.log(locations.RESULTS.length);
 
-		$('#weather-results').removeClass('hide-results').addClass('show-results');	
-	})
+		    for (var i = 0; i < locations.RESULTS.length; i++) { 
+		    	//console.log(locations.RESULTS[i].name);
+		        cityName = locations.RESULTS[i].name;
+		    	line = locations.RESULTS[i].l;
+		    	console.log(cityName);
+		    	console.log(line);
+		    	$('ul#city-results').append('<li><a>' +cityName+ '</a></li>');
+		    	$('#city-results li a').attr('href',line);
+			    $('#results').removeClass('hide-results').addClass('show-results');
+		    }
+		});
+	}		
+
+	function getWeather(){
+		$('#city-results li').on('click',function(e){
+			e.preventDefault();
+			var cityLocation = $(this).attr('href');
+			console.log('cityLocation');
+			// var element = $('#weather-results');
+			// var key = '6d21846ad7649b70';
+			// var weather = "http://api.wunderground.com/api/"+key+"/conditions/q/"+cityLocation+".json";
+			// console.log(weather);
+			// $.ajax({
+			// 	  url : Weather,
+			// 	  dataType : "jsonp",
+			// 	  success : function(results) {
+			// 	  var location = results.current_observation.display_location.full;
+			// 	  console.log(location);
+			// 	  var temp = results.current_observation.feelslike_f;
+			// 	  console.log(temp);
+			// 	  var img = results.current_observation.icon_url;
+			// 	  element.find('#location-results').html(location);
+			// 	  element.find('#icon-results').attr('src', img);
+			// 	  element.find('#temp-results').html(temp);
+			//   	}
+		    // });
+	    $('#weather-results').removeClass('hide-results').addClass('show-results');
+	});
+	}
+
 
 	$('.button-results').on('click',function(){
 		$('#weather-results').removeClass('show-results').addClass('hide-results');
